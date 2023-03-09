487604ab5c99d84a5fc640ba3f147469d353fcc5
HenriqueMonteiro <Henriquepupet@gmail.com>
2023-03-09 21:43:57 +0000

    Changed normal pi for Math.PI

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -10,7 +10,7 @@ namespace PerguntaValores
             /// V = πr2h Sa = 2πr(r + h) - Volume and Radius
             /// </summary>
            
-            float pi = 3.1415926f;
+            //float pi = 3.1415926f;
 
             Console.WriteLine("What is the height (h) of the cylinder: ");
             float h = Convert.ToInt32(Console.ReadLine());
@@ -18,12 +18,12 @@ namespace PerguntaValores
             Console.WriteLine("What is the radius (r) of the cylinder: ");
             float r = Convert.ToInt32(Console.ReadLine());
 
-            double v = (pi*(r * r)*h);
+            double v = (Math.PI*(r * r)*h);
 
-            double s = 2*(pi*r*(r + h));
+            double s = 2*(Math.PI*r*(r + h));
 
-            Console.WriteLine("V = " + v);
-            Console.WriteLine("S = " + s);
+            Console.WriteLine($"V = {v}");
+            Console.WriteLine($"S = {s}");;
 
 
         }
