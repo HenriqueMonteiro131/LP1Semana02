9f54f816d1f325c22756f59bbfff2ade34edc6c0
HenriqueMonteiro <Henriquepupet@gmail.com>
2023-03-09 19:50:33 +0000

    Added the input of the height and radius

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the height (h) of the cylinder: ");
+            string height = Console.ReadLine();
+
+            Console.ReadLine("What is the radius (r) of the cylinder: ");
+            string radius = Console.ReadLine();
+
+            
+        }
+    }
+}
