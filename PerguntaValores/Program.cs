a8e5444adad19962505f74b91325c00b3d996998
HenriqueMonteiro <Henriquepupet@gmail.com>
2023-03-09 19:55:52 +0000

    Fixed The Write and Read Lines

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -7,12 +7,12 @@ namespace PerguntaValores
         static void Main(string[] args)
         {
             Console.WriteLine("What is the height (h) of the cylinder: ");
-            string height = Console.ReadLine();
+            int height = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadLine("What is the radius (r) of the cylinder: ");
-            string radius = Console.ReadLine();
+            Console.WriteLine("What is the radius (r) of the cylinder: ");
+            int radius = Convert.ToInt32(Console.ReadLine());
 
-            
+
         }
     }
 }
