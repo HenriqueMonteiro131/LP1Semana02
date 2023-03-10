4b46e2be690cf7f7a26171d4c793e376d133d19b
HenriqueMonteiro <Henriquepupet@gmail.com>
2023-03-10 13:02:42 +0000

    Added all the known max and minimum values

---
+++ Specials/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Specials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            sbyte sb = sbyte.MaxValue;
+            Console.WriteLine(sb);
+
+            /// <summary>
+            /// mini - minimum value 
+            /// </summary>
+            sbyte sbmini = sbyte.MinValue;
+            Console.WriteLine(sbmini);
+
+            short sh = short.MaxValue;
+            Console.WriteLine(sh);
+
+            short shmini = short.MinValue;
+            Console.WriteLine(shmini);
+
+            double db = double.MaxValue;
+            Console.WriteLine(db);
+            
+            double dbmini = double.MinValue;
+            Console.WriteLine(dbmini);
+
+            float f = float.MaxValue;
+            Console.WriteLine(f);
+            
+            float fmini = float.MinValue;
+            Console.WriteLine(fmini);
+
+            int i = int.MaxValue;
+            Console.WriteLine(i);
+            
+            int imini = int.MinValue;
+            Console.WriteLine(imini);
+
+            decimal dc = decimal.MaxValue; 
+            Console.WriteLine(dc);
+
+            decimal dcmini = decimal.MinValue; 
+            Console.WriteLine(dcmini);
+
+            
+        }
+    }
+}
